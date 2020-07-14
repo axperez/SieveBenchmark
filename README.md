106953db1b703a422b03c458e8209f40293d29f8
axperez <axelperez97@gmail.com>
2020-07-13 17:20:20 -0700

    Add comments, README, and error handling

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # SieveBenchmark
+Count prime numbers up to input n, given by command line argument.
+
+## Instructions:
+1. git clone https://github.com/axperez/SieveBenchmark.git
+2. `cd SieveBenchmark/`
+3. `make clean all`
+4. Run with command line argument as n (number )
+    * `./sieve n`
+
+
+## Sieve Algorithm:
+1. If n ≤ N and n is not prime, then n must be divisible by a prime not exceeding
+√N. We first list all the integers between 2 and N : 
+
+        2,3,4,5,...N
+
+2. We then successively remove the following:
+    * 4, 6, 8, 10, . . . , that is even integers from 22 onwards;
+
+    * 9,15,21,27,..., that is all multiples of 3 from 32 onwards; 
+    * 25, 35, 55, 65, . . . , that is all multiples of 5 from 52 onwards;
+
+3. Continuing in this way we remove all those integers which are multiples of a prime not exceeding √N.
+
+4. The remaining numbers are all the prime numbers not exceeding N.
+
+## Input and Output:
+**Input to the Algorithm:** A natural number N.
+
+**Output of the Algorithm:** For the specified value of N obtain the following values:
+1. The number of primes less than or equal to N. That is, the number π(N).
+2. Therealnumber β(N).
+3. The number of addition operations: A(N).
+4. The number of multiplication operations M(N). Consider squaring to be a multi- plication operation.
+5. Use a clock function to determine the time used in finding the prime numbers. Call it T(N). Initialize a counter with a clock at the beginning of the routine, and note the clock value at the end of the computations. And then take the difference. Use any unit of time.
--- sieve.cpp
+++ sieve.cpp
@@ -1,38 +1,54 @@
 // =========================================================
 //      sieve.cpp
 // 
-//      Benchmark algorithm: Return list of prime numbers up
-//                           to and including n.
+//      Sieve of Erastosthenes Becnhmark: 
+//          Count prime numbers up to input n, given by 
+//          command line arg
 // ---------------------------------------------------------
 // Author:      Axel Perez
 // Date:        7/1/20
-// Runtime:     O()
+// Runtime:     O(NUM_ITERS*n*log(log(n)))
 // =========================================================
+
+// Included Libraries
 #include <cmath>
 #include <tuple>
 #include <chrono>
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <regex>
 
 using namespace std;
 
-
+// Constant
 const unsigned int NUM_ITERS = 10;
 
+//  Sieve of Erastosthenes Algorithm:
+//      Counts the amount of prime numbers, additions, and multiplications less than
+//      or equal to input n. Also calculates B(N) and times the algorithm.
+//
+//      Input:
+//          unsigned int n: Natural number to get primes up to.
+//
+//      Output:
+//          tuple<unsigned int,     : Number of primes less than or equal to input n
+//                double,           : B(N)
+//                unsigned int,     : Number of additions
+//                unsigned int,     : Number of multiplications
+//                double>           : Time elapsed in milliseconds
 tuple<unsigned int, double, unsigned int, unsigned int, double> sieve(unsigned int n) {
     auto start = chrono::high_resolution_clock::now();
-    vector<bool> isPrime(n+1, true);
+    vector<bool> isPrime(n+1, true);    // Using index as actual number
     unsigned int numPrimes = 0, numAdds = 0, numMults = 0, i, k;
     
- 
-    i = 2;
+    i = 2;  // start at 2 (0 and 1 are not primes by definition)
     numMults++;
     while (i*i <= n) {
         if (isPrime[i]) {
             k = i + i;
             numAdds++;
-
+            // If prime, set all multiples of prime to false
             while (k <= n) {
                 isPrime[k] = false;
                 k = k + i;
@@ -43,7 +59,7 @@ tuple<unsigned int, double, unsigned int, unsigned int, double> sieve(unsigned i
         numAdds++;
         numMults++;
     }
-    
+    // Count all the prime numbers left
     for (i = 2; i < n; i++, numAdds++) {
         if (isPrime[i]) {
             numPrimes++;
@@ -56,6 +72,13 @@ tuple<unsigned int, double, unsigned int, unsigned int, double> sieve(unsigned i
     return make_tuple(numPrimes, (numPrimes/(n/log(n))), numAdds, numMults, dur.count());
 }
 
+//  Sieve Benchmark Test:
+//      Runs the sieve algorithm numIters times and prints the average time elapsed
+//      with number of primes, B(N), number of additions, and number of multiplications.
+//
+//      Input:
+//          unsigned int n: Natural number to get primes up to.
+//          unsigned int numIters: Number of iterations to run sieve algorithm.
 void sieveBenchmark(unsigned int n, unsigned int numIters) {
     double microsecSum = 0;
     tuple<unsigned int, double, unsigned int, unsigned int, double> output;
@@ -66,7 +89,7 @@ void sieveBenchmark(unsigned int n, unsigned int numIters) {
     }
 
     cout << "\nNumber of Primes: " << get<0>(output) << endl;
-    cout << "B(n) = " << get<1>(output) << endl;
+    cout << "B(N) = " << get<1>(output) << endl;
     cout << "Number of Addition Operations: " << get<2>(output) << endl;
     cout << "Number of Multiplication Operations: " << get<3>(output) << endl;
     cout << "Avergae Time Elapsed (Average of " << numIters << " Iterations): ";
@@ -74,8 +97,9 @@ void sieveBenchmark(unsigned int n, unsigned int numIters) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        cout << "Error: Include unsigned integer as command line argument." << endl;
+    // Error handling
+    if (argc != 2 || !regex_match(argv[1], regex("[1-9](\\d)*"))) {
+        cout << "Error: Include one natural number (integer greater than zero) as command line argument." << endl;
         return 1;
     }
     
